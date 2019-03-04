4e941840ce0dfa890897cbc3192415d057a72701
Eyeball Edward <edwardyeung.private@gmail.com>
2019-03-04 17:55:13 +1100

    extend bookmarks to train and trams

--- application/routes/Bookmarks.js
+++ application/routes/Bookmarks.js
@@ -6,6 +6,16 @@ router.get('/', (req, res) => {
     res.render('bookmarks/index');
 });
 
+function getBookmarks(db, collection, bookmarkCodes, callback) {
+    db.getCollection(collection).findDocuments({
+        $or: bookmarkCodes.map(bookmarkCode => {
+            return { bookmarkCode };
+        })
+    }).toArray((err, stops) => {
+        callback(stops)
+    });;
+}
+
 router.post('/', (req, res) => {
     let bookmarkCodes = req.body.codes;
     if (!bookmarkCodes || !bookmarkCodes.length) {
@@ -14,13 +24,32 @@ router.post('/', (req, res) => {
         return;
     }
 
-    res.db.getCollection('bus stops').findDocuments({
-        $or: bookmarkCodes.map(bookmarkCode => {
-            return { bookmarkCode };
-        })
-    }).toArray((err, busStops) => {
-        res.render('search/results', {busStops, busServices: [], trainStations: []});
-    });;
+    let bookmarkTypes = {
+        busServices: '',
+        busStops: 'bus stops',
+        trainStations: 'train stations',
+        tramStops: 'tram stops'
+    };
+    let allBookmarks = {};
+    let promises = [];
+
+    Object.keys(bookmarkTypes).forEach(bookmarkKey => {
+        if (!bookmarkTypes[bookmarkKey].length) {
+            allBookmarks[bookmarkKey] = [];
+            return;
+        }
+
+        promises.push(new Promise(resolve => {
+            getBookmarks(res.db, bookmarkTypes[bookmarkKey], bookmarkCodes, bookmarks => {
+                allBookmarks[bookmarkKey] = bookmarks;
+                resolve();
+            });
+        }));
+    });
+
+    Promise.all(promises).then(() => {
+        res.render('search/results', allBookmarks);
+    });
 
 });
 
