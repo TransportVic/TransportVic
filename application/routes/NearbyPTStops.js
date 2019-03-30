d1ae1ada01ca7eebad8a3ec6c4fee57e6a869768
TransportVic <unikittypteltd@gmail.com>
2019-03-31 09:47:54 +1100

    nearby pt stops extended to tram and trains

--- application/routes/NearbyPTStops.js
+++ application/routes/NearbyPTStops.js
@@ -22,8 +22,37 @@ function getNearbyStops(type, db, position, callback) {
 }
 
 router.post('/', (req, res) => {
-    getNearbyStops('bus stops', res.db, req.body, busStops => {
-        res.render('nearby/results', {busStops, position: req.body});
+    let stopTypes = {
+        busStops: 'bus stops',
+        trainStations: 'train stations',
+        tramStops: 'tram stops'
+    };
+    let stopList = {};
+    let promises = [];
+
+
+    Object.keys(stopTypes).forEach(stopType => {
+        promises.push(new Promise(resolve => {
+            getNearbyStops(stopTypes[stopType], res.db, req.body, stops => {
+                stopList[stopType] = stops;
+                resolve();
+            });
+        }));
+    });
+
+    Promise.all(promises).then(() => {
+        let allStops = [];
+        Object.keys(stopTypes).forEach(stopType => {
+            allStops = allStops.concat(stopList[stopType].map(stop => {
+                stop.stopType = stopType;
+                return stop;
+            }));
+        });
+
+        res.render('nearby/results', {
+            position: req.body,
+            allStops
+        });
     });
 });
 
