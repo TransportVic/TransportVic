9b3b980f94a84560d98dcb32b84de7f8c694f9a4
TransportVic <unikittypteltd@gmail.com>
2019-04-04 15:44:41 +1100

    show city loop details for up svc

--- application/timings/metro-timings.js
+++ application/timings/metro-timings.js
@@ -3,6 +3,26 @@ const TimedCache = require('timed-cache');
 
 let timingsCache = new TimedCache({ defaultTtl: 1000 * 60 * 1 });
 
+let cityLoopStations = ['southern cross', 'parliament', 'flagstaff', 'melbourne central'];
+
+class TrainRun {
+    constructor(runData) {
+        let runID = [];
+        if (runData.vehicle_descriptor)
+            runID = [...runData.vehicle_descriptor.id].map(e => parseInt(e));
+
+        this.destination = runData.destination_name;
+        this.expressStopsCount = runData.express_stop_count;
+        this.trainType = (runData.vehicle_descriptor || {}).description;
+
+        this.throughCityLoop = runID[1] > 4 || cityLoopStations.includes(this.destination.toLowerCase());
+        this.stopsViaFlindersFirst = runID[1] <= 4;
+        this.upService = !(runID[3] % 2);
+
+        this.runID = runID.join('');
+    }
+}
+
 function getTimings(trainStationID, db, callback) {
     if (timingsCache.get(trainStationID)) {
         callback(timingsCache.get(trainStationID));
@@ -19,19 +39,18 @@ function getTimings(trainStationID, db, callback) {
             promises.push(new Promise(resolve => {
                 db.getCollection('train lines').findDocument({ ptvRouteID: departure.route_id }, (err, lineData) => {
                     let platform = departure.platform_number;
-                    let runData = data.runs[departure.run_id];
-                    let destination = runData.destination_name;
-                    let directionID = runData.direction_id;
+                    let runData = new TrainRun(data.runs[departure.run_id]);
+                    let destination = runData.destination;
+                    let directionID = data.runs[departure.run_id].direction_id;
                     let arrivalTime = new Date(departure.estimated_departure_utc || departure.scheduled_departure_utc);
                     let reachedPlatform = departure.at_platform;
-                    let trainType = (runData.vehicle_descriptor || {}).description;
 
                     if (departure.route_id === 13) {
                         if (trainStationID === 1073) platform = 3;
                         else platform = 1;
                     }
 
-                    if (new Date() - arrivalTime > 1000 * 60 * 1 || arrivalTime - new Date() > 1000 * 60 * 60 * 3 || platform == null) { // train arrives beyond 3hrs
+                    if (new Date() - arrivalTime > 1000 * 60 * 1.5 || arrivalTime - new Date() > 1000 * 60 * 60 * 3 || platform == null) { // train arrives beyond 3hrs
                         resolve();
                         return;
                     }
@@ -50,8 +69,7 @@ function getTimings(trainStationID, db, callback) {
                         arrivalTime,
                         headwayDeviance,
                         reachedPlatform,
-                        trainType,
-                        hasExpress: !!runData.express_stop_count
+                        runData
                     });
 
                     timings = timings.sort((a, b) => a.arrivalTime - b.arrivalTime);
