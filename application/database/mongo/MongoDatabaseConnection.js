46360a320513b33a9930f810d8a7bc545feda0f9
TransportVic <unikittypteltd@gmail.com>
2019-08-24 21:01:52 +1000

    update mongodb

--- application/database/mongo/MongoDatabaseConnection.js
+++ application/database/mongo/MongoDatabaseConnection.js
@@ -1,34 +1,36 @@
-const MongoDB = require('mongodb');
-const {MongoClient} = MongoDB;
+const MongoDB = require('mongodb')
+const { MongoClient } = MongoDB
 
-const DatabaseCollection = require('./MongoDatabaseCollection');
+const DatabaseCollection = require('./MongoDatabaseCollection')
 
 module.exports = class MongoDatabaseConnection {
+  constructor (databaseURL, databaseName) {
+    this.databaseURL = databaseURL
+    this.databaseName = databaseName
+  }
 
-    constructor(databaseURL, databaseName) {
-        this.databaseURL = databaseURL;
-        this.databaseName = databaseName
+  connect (options, callback) {
+    if (callback == null) {
+      callback = options
     }
 
-    connect(options, callback) {
-        if (callback == null) {
-            callback = options;
-        }
+    return new Promise(resolve => {
+      MongoClient.connect(this.databaseURL, Object.assign(options, { useNewUrlParser: true, useUnifiedTopology: true }), (err, client) => {
+        this.database = client.db(this.databaseName)
 
-        MongoClient.connect(this.databaseURL, Object.assign(options, { useNewUrlParser: true }), (err, database) => {
-            this.database = database.db(this.databaseName);
+        resolve()
+        if (typeof callback == 'function')
+          callback(err)
+      })
+    })
+  }
 
-            callback(err);
-        });
-    }
+  createCollection (collectionName, options) {
+    this.database.createCollection(collectionName, options)
+    return this.getCollection(collectionName)
+  }
 
-    createCollection(collectionName, options) {
-        this.database.createCollection(collectionName, options);
-        return this.getCollection(collectionName);
-    }
-
-    getCollection(collectionName) {
-        return new DatabaseCollection(this.database.collection(collectionName));
-    }
-
+  getCollection (collectionName) {
+    return new DatabaseCollection(this.database.collection(collectionName))
+  }
 }
